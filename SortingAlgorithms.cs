8ec2d54b203f81db01217cb25ca9a20ba3ede05f
Laszlo Palcso <42605968+Laddd80@users.noreply.github.com>
2019-02-22 10:59:04 +0100

    DeleteQuicksort
    Quicksort is in its own project

--- SortingAlgorithms.cs
+++ SortingAlgorithms.cs
@@ -140,43 +140,6 @@ namespace SortingAlgorithms
                 i++;
                 k++;
             }
-        }
-
-        /*Quick Sort
-         * 
-         */
-        static void QuickSort(int[] arr, int left, int right)
-        {
-            int l = left;
-            int r = right;
-            int size = right - left;
-
-            if (size > 1)
-            {
-                Random rand = new Random();
-                int pivot = arr[rand.Next(0, size) + 1];
-
-                while (l < r)
-                {
-                    while (arr[r] > pivot && r > l)
-                    {
-                        r--;
-                    }
-                    while (arr[l] < pivot && l <= r)
-                    {
-                        l++;
-                    }
-                    if (l < r)
-                    {
-                        int temp = arr[l];
-                        arr[l] = arr[r];
-                        arr[r] = temp;
-                        l++;
-                    }
-                }
-                QuickSort(arr, left, l);
-                QuickSort(arr, r, right);
-            }
-        }
+        }      
     }
 }
